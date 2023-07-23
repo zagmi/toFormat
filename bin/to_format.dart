ac1281641e759baa0dd8edd9c5c2d38b3b14d6b8
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-07-22 23:24:07 -0400

    Update to_format.dart

--- bin/to_format.dart
+++ bin/to_format.dart
@@ -1,14 +1,18 @@
 import 'package:to_format/to_format.dart';
 
 void main() {
-  final value = 123456789.123456789;
-  final formatter = ToFormat(value, format: {
-    'decimalSeparator': ',',
-    'groupSeparator': '.',
-    'groupSize': 3,
-    'fractionGroupSeparator': '-',
-    'fractionGroupSize': 2,
-  });
-  final formattedValue = formatter.toFormat(dp: 2, rm: 0);
-  print(formattedValue); 
+  // final value = 123456789.123456789;
+  // final formatter = ToFormat(value, format: {
+  //  'decimalSeparator': ',',
+  //  'groupSeparator': '.',
+  //  'groupSize': 3,
+  //  'fractionGroupSeparator': '-',
+  //  'fractionGroupSize': 2,
+  // });
+  // final formattedValue = formatter.toFormat(dp: 2, rm: 0);
+  // print(formattedValue);
+
+  num x = 1234567.89;
+  String formatted = x.toFormat(dp: 2, fmt: {'groupSeparator': ' '});
+  print(formatted);
 }
